1acb3ec577be2651a4cb3d9f03680cd154008bec
Ysnv <100954636@qq.com>
2020-03-29 14:30:31 +0800

    1.0

---
+++ YITU-UPLOADER-WIDGETS.php
@@ -0,0 +1,55 @@
+<?php
+class qcgzxw extends WP_Widget {
+
+    function __construct() { 
+		 parent::__construct( 
+		 // 小工具ID
+		 'smms-uploader', 
+		 // 小工具名称
+		 __('图床', 'qcgzxw-smms' ), 
+		 // 小工具选项
+		 array (
+		 'description' => __( 'sm.ms图床小工具', 'qcgzxw-smms' )
+		 ) 
+		 ); 
+}
+    function form( $instance ) {
+		@$title = esc_attr( $instance['title'] );
+		?>
+			<p>
+                <label for="<?php echo $this->get_field_id( 'title' ); ?>">
+                    SM图床标题：
+                    <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>" />
+                </label>
+            </p>
+		<?php
+    }
+    function update( $new_instance, $old_instance ) {       
+		return $new_instance;
+    }
+ 
+    function widget( $args, $instance ) {
+		extract( $args );
+		$title = $instance['title'] ? $instance['title'] : 'SM 图床';
+		if($title) $result .= $before_widget . $before_title . $title . $after_title; 
+		echo $result;
+		?>
+			<div class="upload_box">
+			<a href="javascript:;" class="smms-upload-file">选择文件<input type="file" id="image" accept="image/*" multiple="multiple" ></a>
+				<div id = "show" style="widht:100%;height:100%;word-wrap: break-word">
+				<p class = "text-justify" id="urls"></p>
+					<div id = "mul-image"></div>
+				</div>
+			</div>
+			<style>.smms-upload-file{position:relative;display:inline-block;background:#D0EEFF;border:1px solid #99D3F5;border-radius:4px;padding:4px 12px;overflow:hidden;color:#1E88C7;text-decoration:none;text-indent:0;line-height:20px}
+.smms-upload-file input{position:absolute;font-size:100px;right:0;top:0;opacity:0}
+.smms-upload-file:hover{background:#AADFFD;border-color:#78C3F3;color:#004974;text-decoration:none}</style>
+		<?php
+		echo $after_widget;
+    }
+}
+function qcgzxw(){
+	register_widget('qcgzxw');
+}
+add_action('widgets_init','qcgzxw');
+?>
---
+++ yitu-uploader.php
@@ -0,0 +1,161 @@
+<?php
+/*
+Plugin Name: 益图图床外链小工具
+Plugin URI: http://ishanran.com/yitu/
+Description: 山然小站独自开发的图床插件，用于WordPress博客添加 图床上传小工具、评论处图片上传按钮、文章编辑处图片上传按钮。
+Author: 山然小站
+Author URI: http://ishanran.com/
+Version: 1.0
+*/
+add_filter('use_block_editor_for_post', '__return_false');
+define('YITU_URL', plugin_dir_url(__FILE__));
+define('YITU_VERSION', "1.0");
+define('VERSION_CHECK_URL', "https://freed.ga/wp-widgets-info/qcgzxw-smms.json");
+include("YITU-UPLOADER-WIDGETS.php");
+include("YITU-UPLOADER-COMMENTS.php");
+function qcgzxw_scripts_css()
+{
+	wp_deregister_script('jquery');
+	wp_register_script('jquery', YITU_URL . 'js/jquery.min.js', YITU_VERSION);
+	wp_enqueue_script('jquery');
+	if (is_single() || is_page() || is_home()) wp_enqueue_style('bootstrap', YITU_URL . 'css/bootstrap.min.css', array(), YITU_VERSION);
+}
+
+$Uploader = get_option('YITU_DATA');
+echo "<script> var infoData={'Token':'".$Uploader['Token']."'}</script>";
+function admin_scripts_css()
+{
+	wp_enqueue_script('admin-content-js', YITU_URL . 'js/content.min.js', array(), YITU_VERSION, true);
+	wp_enqueue_style('admin-content-css', YITU_URL . 'css/input.min.css', array(), YITU_VERSION);
+}
+add_action('wp_enqueue_scripts', 'qcgzxw_scripts_css');
+//功能启用
+if ($Uploader['Content']) {
+	add_action('admin_head', 'admin_scripts_css');
+	add_action('media_buttons', 'admin_upload_img');
+}
+if ($Uploader['Donate']) {
+	add_action('wp_footer', 'donate');
+}
+//END
+//支持作者
+function donate()
+{
+	echo '<!-- 益图图床WordPress工具，www.htm.fun -->';
+	wp_enqueue_script('smms-donate', YITU_URL . 'js/donate.js', array(), YITU_VERSION, true);
+}
+//插件更新检测
+function update()
+{
+	$response = wp_remote_get(VERSION_CHECK_URL);
+	if (is_array($response) && !is_wp_error($response) && $response['response']['code'] == '200') {
+		$body = json_decode($response['body']);
+	}
+	return $body;
+}
+//添加链接
+function YITU_UPLOADER_LINKS($actions, $plugin_file)
+{
+	static $plugin;
+	if (!isset($plugin))
+		$plugin = plugin_basename(__FILE__);
+	if ($plugin == $plugin_file) {
+		$settings	= array('settings' => '<a href="options-general.php?page=YITU-UPLOADER-OPTIONS">插件设置</a>');
+		$site_link	= array('support' => '<a href="https://github.com/qcgzxw/YITU-UPLOADER" target="_blank">使用说明</a>');
+		$actions 	= array_merge($settings, $actions);
+		$actions	= array_merge($site_link, $actions);
+	}
+	return $actions;
+}
+add_filter('plugin_action_links', 'YITU_UPLOADER_LINKS', 10, 2);
+//默认数据
+add_action('admin_init', 'YITU_options_default_options');
+function YITU_options_default_options()
+{
+	$Uploader = get_option('YITU_DATA'); //获取选项
+	if ($Uploader == '') {
+		$Uploader = array( //设置默认数据
+			'Content' => '',
+			'Donate' => '',
+			'Token' => '',
+		);
+		update_option('YITU_DATA', $Uploader); //更新选项   
+	}
+}
+
+//设置菜单
+function my_plugin_menu()
+{
+	add_options_page('YITU-UPLOADER设置页面', 'SM图床设置', 'manage_options', 'YITU-UPLOADER-OPTIONS', 'my_plugin_options');
+}
+add_action('admin_menu', 'my_plugin_menu');
+function my_plugin_options()
+{
+	if (isset($_POST['Update'])) {
+		$date = update();
+		$ver = $date->ver;
+		if ($ver > YITU_VERSION) {
+			$url = $date->url;
+			$content = $date->content;
+
+			echo '<div class="notice notice-warning"><p>YITU-UPLOADER 插件已经有新版本啦！ <a target="_blank" href="' . $url . '">立即下载</a></p><p><strong>更新内容：</strong>' . $content . '</p></div>';
+		} else {
+			echo '<div class="updated" id="message"><p>暂无更新</p></div>';
+		}
+	}
+	if (isset($_POST['DataSubmit'])) {
+		$Uploader = array(
+			'Content' => trim(@$_POST['content']),
+			'Token' => trim(@$_POST['Token']),
+			'Donate' => trim(@$_POST['donate']),
+		);
+		@update_option('YITU_DATA', $Uploader);
+		add_action('widgets_init', 'qcgzxw');
+		echo '<div class="updated" id="message"><p>提交成功</p></div>';
+	} else {
+		if (!isset($_POST['Update'])) {
+			echo '<div class="updated subscribe-main" id="message"><p>创作不易，给GitHub点个Star吧。——<span class="text-ruo">[<a href="https://github.com/qcgzxw/YITU-UPLOADER" target="_blank">立即前往</a>]</span><i class="fr fb f20 qcgzxw-close">&#215;</i></p>';
+			echo "</div><style>.fb{font-weight:bold;}.f12{font-size:12px;}..f16{font-size:16px;}.f18{font-size:18px;}..fl{float:left;}.fr{float:right;margin-top:-2px;}.oh{overflow:hidden;}i{font-style:normal;}.color-primary{color:#337ab7;}.color-success{color:#5cb85c;}.color-info{color:#5bc0de;}.color-warning{color:#f0ad4e;}.color-red{color:red;padding:0 3px;}</style><script>jQuery('.qcgzxw-close').click(function() {jQuery('.subscribe-main').fadeOut('slow',function(){jQuery('.subscribe-main').remove();});});</script>";
+		}
+	}
+	$Uploader = get_option('YITU_DATA');
+	$Content	= $Uploader['Content']	!== '' ? 'checked="checked"' : '';
+	$Token	= $Uploader['Token'];
+	$Donate	= $Uploader['Donate']	!== '' ? 'checked="checked"' : '';
+
+	if (!current_user_can('manage_options')) {
+		wp_die(__('You do not have sufficient permissions to access this page.'));
+	}
+	echo '<div class="wrap">';
+	echo '<h2>YITU-UPLOADER 插件设置</h2>';
+	echo '<p>&nbsp;&nbsp;YITU-UPLOADER是一款为WordPress添加上传图片小工具以及评论处图片上传按钮的插件！</p>';
+	echo '<form method = "post">';
+	echo '<table class = "form-table">';
+	echo '<tbody>';
+
+	echo '<tr valign="top">';
+	echo '<th scope="row">Token</th>';
+	echo '<td><label><input value = "'.$Token.'" type = "input" name = "Token">  <a href="https://htm.fun/user" target="target">点击获取Token</a></label></td>';
+	echo '</tr>';
+
+	echo '<tr valign="top">';
+	echo '<th scope="row">后台文章编辑启用图片上传</th>';
+	echo '<td><label><input value = "true" type = "checkbox" name = "content" ' . $Content . '>  勾选后在后台文章编辑处自动添加图片上传按钮</label></td>';
+	echo '</tr>';
+
+	echo '<tr valign="top">';
+	echo '<th scope="row">支持作者</th>';
+	echo '<td><label><input value = "true" type = "checkbox" name = "donate" ' . $Donate . '><a title="在wp_footer加入作者的链接。感谢！">[?]</a></label></td>';
+	echo '</tr>';
+
+
+	echo '</tbody>';
+	echo '</table>';
+	echo '<p class = "submit">';
+	echo '<input class = "button button-primary" type = "submit" name = "DataSubmit" id = "submit" value = "保存更改" />&nbsp;&nbsp;&nbsp;&nbsp;';
+	echo '<input class = "button" type = "submit" name = "Update" id = "update" value = "检测更新" />';
+	echo '</p>';
+
+	echo '</table>';
+echo '</div>';
+}

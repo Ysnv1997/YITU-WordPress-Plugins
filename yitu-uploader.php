1985f775c3f69cc2e72528c06ff25909dd8de05f
Ysnv <100954636@qq.com>
2020-03-29 14:55:27 +0800

    修改

--- yitu-uploader.php
+++ yitu-uploader.php
@@ -10,8 +10,8 @@ Version: 1.0
 add_filter('use_block_editor_for_post', '__return_false');
 define('YITU_URL', plugin_dir_url(__FILE__));
 define('YITU_VERSION', "1.0");
-define('VERSION_CHECK_URL', "https://freed.ga/wp-widgets-info/qcgzxw-smms.json");
-include("YITU-UPLOADER-WIDGETS.php");
+define('VERSION_CHECK_URL', "http://cdn.ishanran.com/yitu-update.json");
+
 include("YITU-UPLOADER-COMMENTS.php");
 function qcgzxw_scripts_css()
 {
@@ -22,7 +22,7 @@ function qcgzxw_scripts_css()
 }
 
 $Uploader = get_option('YITU_DATA');
-echo "<script> var infoData={'Token':'".$Uploader['Token']."'}</script>";
+echo "<script> var infoData={'Token':'" . $Uploader['Token'] . "'}</script>";
 function admin_scripts_css()
 {
 	wp_enqueue_script('admin-content-js', YITU_URL . 'js/content.min.js', array(), YITU_VERSION, true);
@@ -34,16 +34,8 @@ if ($Uploader['Content']) {
 	add_action('admin_head', 'admin_scripts_css');
 	add_action('media_buttons', 'admin_upload_img');
 }
-if ($Uploader['Donate']) {
-	add_action('wp_footer', 'donate');
-}
+
 //END
-//支持作者
-function donate()
-{
-	echo '<!-- 益图图床WordPress工具，www.htm.fun -->';
-	wp_enqueue_script('smms-donate', YITU_URL . 'js/donate.js', array(), YITU_VERSION, true);
-}
 //插件更新检测
 function update()
 {
@@ -61,7 +53,7 @@ function YITU_UPLOADER_LINKS($actions, $plugin_file)
 		$plugin = plugin_basename(__FILE__);
 	if ($plugin == $plugin_file) {
 		$settings	= array('settings' => '<a href="options-general.php?page=YITU-UPLOADER-OPTIONS">插件设置</a>');
-		$site_link	= array('support' => '<a href="https://github.com/qcgzxw/YITU-UPLOADER" target="_blank">使用说明</a>');
+		$site_link	= array('support' => '<a href="http://ishanran.com/yitu/" target="_blank">使用说明</a>');
 		$actions 	= array_merge($settings, $actions);
 		$actions	= array_merge($site_link, $actions);
 	}
@@ -86,7 +78,7 @@ function YITU_options_default_options()
 //设置菜单
 function my_plugin_menu()
 {
-	add_options_page('YITU-UPLOADER设置页面', 'SM图床设置', 'manage_options', 'YITU-UPLOADER-OPTIONS', 'my_plugin_options');
+	add_options_page('益图设置页面', '益图设置', 'manage_options', 'YITU-UPLOADER-OPTIONS', 'my_plugin_options');
 }
 add_action('admin_menu', 'my_plugin_menu');
 function my_plugin_options()
@@ -98,7 +90,7 @@ function my_plugin_options()
 			$url = $date->url;
 			$content = $date->content;
 
-			echo '<div class="notice notice-warning"><p>YITU-UPLOADER 插件已经有新版本啦！ <a target="_blank" href="' . $url . '">立即下载</a></p><p><strong>更新内容：</strong>' . $content . '</p></div>';
+			echo '<div class="notice notice-warning"><p>益图 插件已经有新版本啦！ <a target="_blank" href="' . $url . '">立即下载</a></p><p><strong>更新内容：</strong>' . $content . '</p></div>';
 		} else {
 			echo '<div class="updated" id="message"><p>暂无更新</p></div>';
 		}
@@ -107,35 +99,33 @@ function my_plugin_options()
 		$Uploader = array(
 			'Content' => trim(@$_POST['content']),
 			'Token' => trim(@$_POST['Token']),
-			'Donate' => trim(@$_POST['donate']),
 		);
 		@update_option('YITU_DATA', $Uploader);
 		add_action('widgets_init', 'qcgzxw');
 		echo '<div class="updated" id="message"><p>提交成功</p></div>';
 	} else {
 		if (!isset($_POST['Update'])) {
-			echo '<div class="updated subscribe-main" id="message"><p>创作不易，给GitHub点个Star吧。——<span class="text-ruo">[<a href="https://github.com/qcgzxw/YITU-UPLOADER" target="_blank">立即前往</a>]</span><i class="fr fb f20 qcgzxw-close">&#215;</i></p>';
+			echo '<div class="updated subscribe-main" id="message"><p>创作不易，给GitHub点个Star吧。——<span class="text-ruo">[<a href="https://github.com/Ysnv1997/YITU-WordPress-Plugins" target="_blank">立即前往</a>]</span><i class="fr fb f20 qcgzxw-close">&#215;</i></p>';
 			echo "</div><style>.fb{font-weight:bold;}.f12{font-size:12px;}..f16{font-size:16px;}.f18{font-size:18px;}..fl{float:left;}.fr{float:right;margin-top:-2px;}.oh{overflow:hidden;}i{font-style:normal;}.color-primary{color:#337ab7;}.color-success{color:#5cb85c;}.color-info{color:#5bc0de;}.color-warning{color:#f0ad4e;}.color-red{color:red;padding:0 3px;}</style><script>jQuery('.qcgzxw-close').click(function() {jQuery('.subscribe-main').fadeOut('slow',function(){jQuery('.subscribe-main').remove();});});</script>";
 		}
 	}
 	$Uploader = get_option('YITU_DATA');
 	$Content	= $Uploader['Content']	!== '' ? 'checked="checked"' : '';
 	$Token	= $Uploader['Token'];
-	$Donate	= $Uploader['Donate']	!== '' ? 'checked="checked"' : '';
 
 	if (!current_user_can('manage_options')) {
 		wp_die(__('You do not have sufficient permissions to access this page.'));
 	}
 	echo '<div class="wrap">';
-	echo '<h2>YITU-UPLOADER 插件设置</h2>';
-	echo '<p>&nbsp;&nbsp;YITU-UPLOADER是一款为WordPress添加上传图片小工具以及评论处图片上传按钮的插件！</p>';
+	echo '<h2>益图 插件设置</h2>';
+	echo '<p>&nbsp;&nbsp;益图是一款为WordPress添加上传图片小工具以及评论处图片上传按钮的插件！</p>';
 	echo '<form method = "post">';
 	echo '<table class = "form-table">';
 	echo '<tbody>';
 
 	echo '<tr valign="top">';
 	echo '<th scope="row">Token</th>';
-	echo '<td><label><input value = "'.$Token.'" type = "input" name = "Token">  <a href="https://htm.fun/user" target="target">点击获取Token</a></label></td>';
+	echo '<td><label><input value = "' . $Token . '" type = "input" name = "Token">  <a href="https://htm.fun/user" target="target">点击获取Token</a></label></td>';
 	echo '</tr>';
 
 	echo '<tr valign="top">';
@@ -143,10 +133,6 @@ function my_plugin_options()
 	echo '<td><label><input value = "true" type = "checkbox" name = "content" ' . $Content . '>  勾选后在后台文章编辑处自动添加图片上传按钮</label></td>';
 	echo '</tr>';
 
-	echo '<tr valign="top">';
-	echo '<th scope="row">支持作者</th>';
-	echo '<td><label><input value = "true" type = "checkbox" name = "donate" ' . $Donate . '><a title="在wp_footer加入作者的链接。感谢！">[?]</a></label></td>';
-	echo '</tr>';
 
 
 	echo '</tbody>';
@@ -157,5 +143,5 @@ function my_plugin_options()
 	echo '</p>';
 
 	echo '</table>';
-echo '</div>';
+	echo '</div>';
 }
